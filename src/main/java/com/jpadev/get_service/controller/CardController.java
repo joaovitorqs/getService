20621969b2fd0d1188144fb4062daaef2ecfdf4a
João Vitor <joao_vqs@hotmail.com>
2026-04-16 13:27:54 -0300

    Add package controller and add controller CardController

---
+++ src/main/java/com/jpadev/get_service/controller/CardController.java
@@ -0,0 +1,42 @@
+package com.jpadev.get_service.controller;
+
+import com.jpadev.get_service.model.Card;
+import com.jpadev.get_service.service.CardService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cards")
+public class CardController {
+
+    private final CardService cardService;
+
+    public CardController (CardService cardService){
+        this.cardService = cardService;
+    }
+
+    @GetMapping
+    public List<Card> listAllCards () {
+        return cardService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findCardById(@PathVariable Long id){
+        Card card = cardService.findCardById(id);
+        return ResponseEntity.ok(card);
+    }
+
+    @PostMapping
+    public Card createCard(@RequestBody Card card){
+        return cardService.salvarCard(card);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCard(@PathVariable Long id){
+        cardService.deleteCard(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
