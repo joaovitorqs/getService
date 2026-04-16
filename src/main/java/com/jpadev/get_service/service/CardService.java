3684ee422ad39cbb8a2aa6a2f50ca74cbc85c68b
João Vitor <joao_vqs@hotmail.com>
2026-04-16 10:47:42 -0300

    Add package service and add service CardService

---
+++ src/main/java/com/jpadev/get_service/service/CardService.java
@@ -0,0 +1,40 @@
+package com.jpadev.get_service.service;
+
+import com.jpadev.get_service.model.Card;
+import com.jpadev.get_service.repository.CardRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class CardService {
+
+    private final CardRepository cardRepository;
+
+    public List<Card> listAll(){
+        return cardRepository.findAll();
+    }
+
+    public Card findCardById(Long id){
+        return cardRepository.findById(id)
+                .orElseThrow();//Voltar e tratar exception corretamente !
+    }
+
+    public CardService (CardRepository cardRepository){
+        this.cardRepository = cardRepository;
+    }
+    public Card salvarCard (Card card){
+        card.setCreatedTime(LocalDateTime.now());
+        card.setTimeExpired(LocalDateTime.now().plusDays(3));
+
+        return cardRepository.save(card);
+    }
+
+    public void deleteCard (Long id){
+        if (!cardRepository.existsById(id)){
+            //Voltar e tratar exception corretamente !
+        }
+        cardRepository.deleteById(id);
+    }
+}
